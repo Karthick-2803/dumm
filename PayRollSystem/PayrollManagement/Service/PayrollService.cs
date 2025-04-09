804265988c7cf9d915af3a52f23cbf6588b44154
Karthick-2803 <karthick.m2803@gmail.com>
2025-04-09 21:38:24 +0530

    payroll

---
+++ PayRollSystem/PayrollManagement/Service/PayrollService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayrollManagement.model;
+using PayrollManagement.Repository;
+using PayrollManagement.Exceptions;
+using PayrollManagement.model;
+
+namespace PayrollManagement.Service
+{
+    internal class PayrollService : IPayrollService
+    {
+        readonly IPayrollServiceRepository _payrollServiceRepository;
+        public PayrollService()
+        {
+            _payrollServiceRepository = new PayrollServiceRepository();
+        }
+        public void GetPayrollById()
+        {
+            Console.WriteLine("Enter the payrollId:");
+            int payRollno = int.Parse(Console.ReadLine());
+            List<Payroll> PayrollIdlist = _payrollServiceRepository.GetPayrollById(payRollno);
+            foreach (Payroll payroll in PayrollIdlist)
+            {
+                Console.WriteLine(payroll);
+            }
+        }
+        public void GetPayrollsForEmployee()
+        {
+            Console.WriteLine("Enter the EmployeeId:");
+            int Employeeno = int.Parse(Console.ReadLine());
+            List<Payroll> EmployeeIdlist = _payrollServiceRepository.GetPayrollsForEmployee(Employeeno);
+            foreach (Payroll payroll in EmployeeIdlist)
+            {
+                Console.WriteLine(payroll);
+            }
+        }
+        public void GetPayrollsForPeriod()
+        {
+            Console.WriteLine("Enter the startDate:");
+            DateTime startdate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the endDate:");
+            DateTime enddate = DateTime.Parse(Console.ReadLine());
+            List<Payroll> PayRollsForPeriod = _payrollServiceRepository.GetPayrollsForPeriod(startdate, enddate);
+            foreach (Payroll payroll in PayRollsForPeriod)
+            {
+                Console.WriteLine(payroll);
+            }
+        }
+        public void GeneratePayroll()
+        {
+            Console.WriteLine("Enter the EmployeeId:");
+            int Employeeno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the startDate:");
+            DateTime startdate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the endDate:");
+            DateTime enddate = DateTime.Parse(Console.ReadLine());
+            List<Payroll> PayRoll = _payrollServiceRepository.GeneratePayroll(Employeeno, startdate, enddate);
+            foreach (Payroll payroll in PayRoll)
+            {
+                Console.WriteLine(payroll);
+            }
+        }
+
+    }
+}
---
+++ PayRollSystem/PayrollManagement/model/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollManagement.model
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Mail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Position { get; set; }
+        public DateTime JoiningDate { get; set; }
+
+        private DateTime? _terminationDate;
+        public DateTime? TerminationDate
+        {
+            get { return _terminationDate; }
+            set
+            {
+                if (value.HasValue)
+                {
+                    _terminationDate = (DateTime)value;
+                }
+                else
+                {
+                    _terminationDate = DateTime.MinValue;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"EmployeeID::{EmployeeID}\t FirstName::{FirstName}\t LastName::{LastName}\t DateOfBirth::{DateOfBirth}\t Gender::{Gender} \t mail::{Mail} \t phoneno:{PhoneNumber}\t Address::{Address} \t Position::{Position} \t JoiningDate::{JoiningDate} \t Terminationdate::{TerminationDate}";
+        }
+
+        public Employee()
+        {
+        }
+
+        public Employee(int employeeID, string firstName, string lastName, DateTime dateOfBirth,
+                       string gender, string mail, string phoneNumber, string address,
+                       string position, DateTime joiningDate, DateTime terminationDate)
+        {
+            EmployeeID = employeeID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            Mail = mail;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            Position = position;
+            JoiningDate = joiningDate;
+            TerminationDate = terminationDate;
+        }
+
+        public int CalculateAge()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - DateOfBirth.Year;
+            if (today < DateOfBirth.AddYears(age))
+                age--;
+            return age;
+        }
+    }
+}
